06ab11207be4e72199f5a3078276edecc76074a6
Sergey E. Koposov <skoposov@ed.ac.uk>
2025-04-15 17:13:26 +0100

    fix authentication

--- _scripts/pub_maker.py
+++ _scripts/pub_maker.py
@@ -7,7 +7,6 @@ import os
 import requests
 import pandas as pd
 from urllib.parse import quote
-from pathlib import Path
 
 
 def get_config():
@@ -17,13 +16,13 @@ def get_config():
     """
     if os.getenv('ADS_TOKEN') is None:
         with open(os.path.expanduser("~/.ads/dev_key")) as f:
-            token = f.read().strip() 
+            token = f.read().strip()
     else:
         token = os.getenv('ADS_TOKEN')
     return {
         "url": "https://api.adsabs.harvard.edu/v1/biblib",
         "headers": {
-            "Authorization": "Bearer:{}".format(token),
+            "Authorization": "Bearer {}".format(token),
             "Content-Type": "application/json",
         },
     }
@@ -36,11 +35,19 @@ def get_bibcodes(library_id):
     config = get_config()
 
     url = f"{config['url']}/libraries/{library_id}"
-    r = requests.get(url,
-                     params={"start": start,
-                             "rows": rows},
-                     headers=config["headers"],
-    )
+    try:
+        r = requests.get(
+            url,
+            params={
+                "start": start,
+                "rows": rows
+            },
+            headers=config["headers"],
+        )
+        r.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the request: {e}")
+
     # Get all the documents that are inside the library
     try:
         bibcodes = r.json()["documents"]
@@ -53,20 +60,13 @@ def get_title_str(pub):
     # This fixes up a bunch of minor formatting issues
     title_str = pub['title'][0]
 
-    things_to_fix = [[r"$\sim$", "~"],
-                     [r"$R$", "*R*"],
-                     [r"[$\alpha/\rm Fe]$", "[α/Fe]"],
-                     [r"$\alpha$", "α"],
-                     [r"∼", "~"],
-                     [r"$< -0.75$", "< −0.75"],
-                     [r"$\textit{TESS}$", "*TESS*"],
-                     [r"$Gaia$", "*Gaia*"],
-                     [r"${S}^5$", "S⁵"],
-                     [r"$S^5$", "S⁵"],
-                     ["(S5)", "(S⁵)"],
-                     ["S<SUP>5</SUP>", "S⁵"],
-                     ["S <SUP>5</SUP>", "S⁵"],
-                     ["*", "\*"]]
+    things_to_fix = [[r"$\sim$", "~"], [r"$R$", "*R*"],
+                     [r"[$\alpha/\rm Fe]$", "[α/Fe]"], [r"$\alpha$", "α"],
+                     [r"∼", "~"], [r"$< -0.75$", "< −0.75"],
+                     [r"$\textit{TESS}$", "*TESS*"], [r"$Gaia$", "*Gaia*"],
+                     [r"${S}^5$", "S⁵"], [r"$S^5$", "S⁵"], ["(S5)", "(S⁵)"],
+                     ["S<SUP>5</SUP>", "S⁵"], ["S <SUP>5</SUP>", "S⁵"],
+                     ["*", r"\*"]]
 
     for thing_to_fix in things_to_fix:
         title_str = title_str.replace(thing_to_fix[0], thing_to_fix[1])
@@ -127,13 +127,20 @@ def link_str(doi_str, arxiv_str):
 def get_data_frame(library_id):
     config = get_config()
     bibcodes = get_bibcodes(library_id)
-    r = requests.post("https://api.adsabs.harvard.edu/v1/search/bigquery",
-                      params={"q": "*:*",
-                              "fl": "bibcode,title,year,bibstem,author_count,volume,pub,page,issue,identifier,author,doi,date,doctype",
-                              "rows": 2000},
-                      headers={'Authorization': config['headers']['Authorization'],
-                               'Content-Type': 'big-query/csv'},
-                      data="bibcode\n" + "\n".join(bibcodes))
+    r = requests.post(
+        "https://api.adsabs.harvard.edu/v1/search/bigquery",
+        params={
+            "q": "*:*",
+            "fl":
+            "bibcode,title,year,bibstem,author_count,volume,pub,page,issue,"
+            "identifier,author,doi,date,doctype",
+            "rows": 2000
+        },
+        headers={
+            'Authorization': config['headers']['Authorization'],
+            'Content-Type': 'big-query/csv'
+        },
+        data="bibcode\n" + "\n".join(bibcodes))
     doc_dict = r.json()['response']['docs']
     pub_df = pd.DataFrame(doc_dict)
     pub_df.fillna(value=" ", inplace=True)
@@ -141,17 +148,19 @@ def get_data_frame(library_id):
     pub_df = pub_df[~pub_df.doctype.isin(ignore_doctype).values]
     return pub_df
 
+
 def create_webpage(library_id, md_pub_file, title, subtitle):
     pub_df = get_data_frame(library_id)
 
-    cwd = Path(__file__).parent
-    img_name = f"{md_pub_file.split('.')[0].split('/')[1]}_number_papers.svg"
+    # cwd = Path(__file__).parent
+    # img_name = f"{md_pub_file.split('.')[0].split('/')[1]}_number_papers.svg"
 
     with open(md_pub_file, 'w') as pub_md:
         year_list = []
         article_list = []
         eprint_list = []
-        for year, year_df in pub_df.sort_values('year', ascending=False).groupby("year", sort=False):
+        for year, year_df in pub_df.sort_values(
+                'year', ascending=False).groupby("year", sort=False):
             year_list.append(int(year))
             year_counts = year_df['doctype'].value_counts()
             if 'article' in year_counts:
@@ -165,14 +174,22 @@ def create_webpage(library_id, md_pub_file, title, subtitle):
             pub_md.write("\n")
             pub_md.write(f"#### {year}\n")
             pub_md.write("\n")
-            for *_, pub in year_df.sort_values(['date', 'bibcode'], ascending=[False, False]).iterrows():
+            for *_, pub in year_df.sort_values(['date', 'bibcode'],
+                                               ascending=[False,
+                                                          False]).iterrows():
                 markdown_str = "* "
-                title_str = f"[**{get_title_str(pub)}**](https://ui.adsabs.harvard.edu/abs/{quote(pub['bibcode'])})"
+                title_str = (
+                    f"[**{get_title_str(pub)}**]"
+                    f"(https://ui.adsabs.harvard.edu/abs/{quote(pub['bibcode'])})"
+                )
                 author_str = get_author_str(pub)
                 year_str = f"({pub['year']})"
                 publication_str = get_pub_vol_pp_str(pub)
 
-                markdown_str = f"* {title_str}<br/>{author_str} {year_str} {publication_str} {link_str(get_doi_str(pub), get_arxiv_str(pub))}\n"
+                markdown_str = (
+                    f"* {title_str}<br/>{author_str} {year_str}"
+                    f" {publication_str} {link_str(get_doi_str(pub), get_arxiv_str(pub))}\n"
+                )
 
                 pub_md.write(markdown_str)
 
